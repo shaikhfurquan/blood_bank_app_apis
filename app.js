e89970dac585395f9a1f1e68fb01c06811681732
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-12 00:44:19 +0530

    morgan pkg

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './db/connectDB.js';
+import morgan from 'morgan';
 dotenv.config()
 
 
 const app = express()
 
+//express middlewares
+app.use(express.json())
+app.use(morgan('dev'))
 
 //DB connection
 connectDB()
