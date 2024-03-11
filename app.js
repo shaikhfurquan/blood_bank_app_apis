fedb56c326160d87e385711b7d5ab23216e831b2
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-12 01:07:04 +0530

    cors pkg

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './db/connectDB.js';
 import morgan from 'morgan';
+import cors from 'cors';
 dotenv.config()
 
 
 const app = express()
 
+const PORT = process.env.PORT || 3500
+
 //express middlewares
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(cors())
 
 //DB connection
 connectDB()
 
-app.listen(process.env.PORT , ()=>{
-    console.log(`Server started at ${process.env.PORT}`);
+app.listen(PORT , ()=>{
+    console.log(`Server started at ${PORT}`);
 })
