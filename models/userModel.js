7d137dac65a65f89ebb3ec737406e174fcf20663
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-13 19:47:09 +0530

    added donar

--- models/userModel.js
+++ models/userModel.js
@@ -6,12 +6,13 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         required: [true, "role is required"],
-        enum: ["admin", "organization", 'hospital', 'user']
+        enum: ["admin", "organization", 'hospital', 'donar'],
+        trim : true,
     },
     name: {
         type: String,
         required: function () {
-            if (this.role === 'user' || this.role === 'admin') {
+            if (this.role === 'donar' || this.role === 'admin') {
                 return true
             }
             return false
@@ -39,21 +40,26 @@ const userSchema = new mongoose.Schema({
         type: String,
         unique: [true, "Please provide a valid email address"],
         required: [true, "Email is required"],
+        trim : true,
     },
     password: {
         type: String,
-        required: [true, "Password is required"]
+        required: [true, "Password is required"],
+        trim : true,
     },
     website: {
         type: String,
+        trim : true,
     },
     address: {
         type: String,
-        required: [true, "Address is required"]
+        required: [true, "Address is required"],
+        trim : true,
     },
     phone: {
         type: String,
-        required: [true, "Phone number is required"]
+        required: [true, "Phone number is required"],
+        trim : true,
     }
 
 }, { timestamps: true })
