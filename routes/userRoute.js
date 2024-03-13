58b8ac8bed2456e4f3a4cef8130767a6e4076edc
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-13 19:46:39 +0530

    current user route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,11 @@
 import express  from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import { getCurrentUser, loginUser, registerUser } from "../controllers/userController.js";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post('/register' , registerUser)
 userRouter.post('/login' , loginUser)
+userRouter.get('/current-user' , isAuthenticated , getCurrentUser)
 
 export default userRouter
